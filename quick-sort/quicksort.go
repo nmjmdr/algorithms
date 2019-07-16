8934147e4488b49b7746f534eebea46c0b9e90a3
nmjmdr <narasimha.gm@gmail.com>
2019-07-16 13:57:31 +1000

    Update quicksort.go

--- quick-sort/quicksort.go
+++ quick-sort/quicksort.go
@@ -1,53 +1,32 @@
-package main
-
-import (
-	"fmt"
-	"math"
-)
-
-func combine(left []int,p int,right []int) []int {
- left = append(left,p)
- return append(left,right...)
+const reorder = (arr, pivot) => {
+  let larger = []
+  let smaller = []
+  let equals = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < arr[pivot]) {
+      smaller.push(arr[i])
+    } else if (arr[i] > arr[pivot]) {
+      larger.push(arr[i])
+    } else {
+      equals.push(arr[i])
+    }
+  }
+  return {
+    smaller: smaller,
+    equals: equals,
+    larger: larger
+  }
 }
 
-func sort(arr []int) []int {
-	pivot := int(math.Floor(float64(len(arr) / 2)))
-	if len(arr) <= 1 {
-	  return arr
-	}
-	arr = reorder(arr, pivot)
-	left := arr[0:pivot]
-	right := []int{}
-	if pivot+1 < len(arr) {
-		right = arr[pivot+1:]
-	}
-	return combine(sort(left),arr[pivot],sort(right))
+
+const sort = (arr) => {
+
+  if (arr.length <= 1) {
+    return arr
+  }
+  const pivot = Math.floor(arr.length / 2)
+  const result = reorder(arr, pivot)
+  return [...sort(result.smaller), ...result.equals, ...sort(result.larger)]
 }
 
-func reorder(arr []int, pivot int) []int {
-	swaps := 0
-	for i := 0; i < pivot; i++ {
-		if arr[i] > arr[pivot] {
-			x := arr[i]
-			arr[i] = arr[pivot]
-			arr[pivot] = x
-			swaps++
-		}
-	}
-	for j := pivot + 1; j < len(arr); j++ {
-		if arr[j] < arr[pivot] {
-			x := arr[j]
-			arr[j] = arr[pivot]
-			arr[pivot] = x
-			swaps++
-		}
-	}
-	if swaps > 0 {
-		arr = reorder(arr, pivot)
-	}
-	return arr
-}
-
-func main() {
-	fmt.Println(sort([]int{9, 4, 1, 3, 8, 14, 12, 15, 10, 2}))
-}
+console.log(sort([3, 1, 1, 1, 2]))
