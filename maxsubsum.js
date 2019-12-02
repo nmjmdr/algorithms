e0b1860ed59d07f7c62777ad952d19d917dcf0d1
Nara <nara@beemit.com.au>
2019-12-02 15:51:48 +1100

    max sub sum

---
+++ maxsubsum.js
@@ -0,0 +1,40 @@
+
+const brute = (arr) => {
+    if(!arr || arr.length == 0) {
+        return null
+    }
+    let maxglobal = Number.MIN_SAFE_INTEGER;
+    for(let index=0;index<arr.length;index++) {
+    let maxcurrent = arr[index]
+    for(let i=index+1;i<arr.length;i++) {
+        maxcurrent = arr[i] + maxcurrent;
+        if(maxcurrent > maxglobal) {
+            maxglobal = maxcurrent;
+        }
+        }
+    }
+    return maxglobal
+}
+
+const kadane = (arr) => {
+    if(!arr || arr.length == 0) {
+        return null
+    }
+    let maxglobal = arr[0]
+    let maxcurrent = arr[0]
+
+    for(let i=1;i<arr.length;i++) {
+        maxcurrent = Math.max(arr[i]+maxcurrent, maxcurrent)
+        if(maxglobal < maxcurrent) {
+            maxglobal = maxcurrent
+        }
+        
+        maxcurrent = arr[i] + maxcurrent
+    }
+    return maxglobal
+}
+
+let arr = [-2,1,-3,4,-1,2,1,-5,4];
+console.log(brute(arr))
+
+console.log(kadane(arr))
